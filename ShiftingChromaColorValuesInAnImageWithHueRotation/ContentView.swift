c7803b21eb946d22084502403877d5c5c6966e75
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-17 11:53:27 +0300

    init

--- ShiftingChromaColorValuesInAnImageWithHueRotation/ContentView.swift
+++ ShiftingChromaColorValuesInAnImageWithHueRotation/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var animateGradient = false
+    @State var shiftColors = false
     var body: some View {
-        Text("Hello World")
+        
+        Image(systemName: "umbrella")
+            .font(.largeTitle)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            //.background(Color(red: 0, green: 0.0, blue: 1, opacity: 0.5))
+            .background(LinearGradient(gradient: Gradient(colors: [.purple, .green, .blue, .gray, .orange, .pink, .purple]), startPoint: animateGradient ? .top : .topTrailing, endPoint: animateGradient ? .bottom : .topLeading).opacity(0.6))
+            .hueRotation(.degrees(shiftColors ? 0 : 360))
+            .animation(Animation.linear(duration: 10).repeatForever(autoreverses: true).speed(1.5))
+            .onAppear() {
+                self.animateGradient.toggle()
+                self.shiftColors.toggle()
+            
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
